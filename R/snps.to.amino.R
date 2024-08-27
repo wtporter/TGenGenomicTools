eb18e3c90cda527e339cc7be3a793e49639a2547
W. Tanner Porter <tporter@tgen.org>
2024-08-27 09:12:33 -0700

    SNPS.to.Amino update

--- R/snps.to.amino.R
+++ R/snps.to.amino.R
@@ -49,6 +49,8 @@ snps.to.amino <- function(snp_db, ref_seq, cores = parallelly::availableCores())
     POSITION <- Amino_Acid_List$snp_position[SNP]
     MUTATION <- Amino_Acid_List$snp_mutation[SNP]
 
+    GENOME_SNP <- paste0(REFERENCE,POSITION,MUTATION)
+
     Out <- data.frame()
 
     if(MUTATION !="_"& nchar(MUTATION) == 1){
@@ -67,6 +69,24 @@ snps.to.amino <- function(snp_db, ref_seq, cores = parallelly::availableCores())
 
           Observed_Seq[SNP_in_gene] <- MUTATION
 
+          Observed_SNP <- Observed_Seq[SNP_in_gene]
+
+
+          ## Add functionality to allow SNPs on second strand.
+          if(Reference_DF$strand[GENE] == "-"){
+
+            Reference_Seq <- Biostrings::reverseComplement(Reference_Seq)
+
+            Observed_Seq <- Biostrings::reverseComplement(Observed_Seq)
+
+            Theoretical_Ref <- Biostrings::reverseComplement(Theoretical_Ref)
+
+            MUTATION <- Biostrings::reverseComplement(Observed_SNP)
+
+            SNP_in_gene <- (Reference_DF$end[GENE] - POSITION) + 1
+
+          }
+
           AA_Seq <- Biostrings::translate(Reference_Seq, if.fuzzy.codon = "solve")
 
           AA_Observed <- Biostrings::translate(Observed_Seq, if.fuzzy.codon = "solve")
@@ -77,33 +97,47 @@ snps.to.amino <- function(snp_db, ref_seq, cores = parallelly::availableCores())
             Biostrings::mismatchTable() %>%
             mutate(AA_Change = paste(Reference_DF$gene[GENE], ":", PatternSubstring, PatternStart, SubjectSubstring, sep = ""))
 
-          Out <- rbind(Out, data.frame("snp_position" = POSITION, "snp_mutation" = MUTATION, "Theoretical_Reference" = Theoretical_Ref, "Gene" = Reference_DF$gene[GENE], "Product" = Reference_DF$product[GENE], "AA" = ifelse(length(as.character(unique(Mutations$AA_Change))) > 0 ,
+          Out <- rbind(Out, data.frame("SNP" = GENOME_SNP,
+                                       "snp_position_genome" = POSITION,
+                                       "snp_position_gene" = SNP_in_gene,
+                                       "snp_mutation" = MUTATION,
+                                       "Theoretical_Reference" = Theoretical_Ref,
+                                       "Gene" = Reference_DF$gene[GENE],
+                                       "Product" = Reference_DF$product[GENE],
+                                       "AA" = ifelse(length(as.character(unique(Mutations$AA_Change))) > 0 ,
                                                                                              as.character(unique(Mutations$AA_Change)),
-                                                                                             "Synonymous")))
+                                                                                             "Synonymous"),
+                                        "SNP_Gene" = paste0(Theoretical_Ref, SNP_in_gene, MUTATION)))
         }
       }
     }
     Out
   }
+
+  Temp
+
   # Stop the parallel cluster
   stopCluster(cl)
 
-  Temp$snp_position <- as.numeric(Temp$snp_position)
-
-  Out <- full_join(Amino_Acid_List, Temp)
+  Out <- full_join(select(Amino_Acid_List, SNP), Temp)
 
   Out$AA[is.na(Out$AA) & nchar(Out$snp_mutation) > 1] <- "Insertions Not Supported"
-
   Out$AA[is.na(Out$AA) & Out$snp_mutation == "_"] <- "Deletions Not Supported"
-
   Out$AA[is.na(Out$AA)] <- "Non-coding SNP"
 
-  Out <- select(Out, SNP, AA, Gene, Product, Theoretical_Reference)
+  Out$SNP_Gene[is.na(Out$SNP_Gene) & nchar(Out$snp_mutation) > 1] <- "Insertions Not Supported"
+  Out$SNP_Gene[is.na(Out$SNP_Gene) & Out$snp_mutation == "_"] <- "Deletions Not Supported"
+  Out$SNP_Gene[is.na(Out$SNP_Gene)] <- "Non-coding SNP"
+
+  Out
+
+
+  Out <- select(Out, SNP, SNP_Gene, AA, Gene, Product, Theoretical_Reference)
 
   Out
 }
 
-# # Example of functionality
+# # # Example of functionality
 # library(TGenGenomicTools)
 # library(genbankr)
 # library(Biostrings)
@@ -112,10 +146,15 @@ snps.to.amino <- function(snp_db, ref_seq, cores = parallelly::availableCores())
 # library(parallel)
 # library(parallelly)
 # library(doParallel)
-# snp_db <- fasta.to.snps(fasta="/scratch/tporter/RSV_20230402_SequencingMethodCompare_SQL/RSV_A_Fasta/TGen_Clinical_RSVA.fasta",
-#                         ref_seq="/scratch/tporter/RSV_20230630_Phylogenetic_Analysis/",
-#                         cores=parallelly::availableCores())
-# snp_db <- read.csv("/scratch/tporter/RSV_20230908_HomoplasticSNPAnalysis/RSV_A_Homoplastic.csv")
-# ref_seq="/scratch/tporter/RSV_20230825_ROI_Formatting/RSV_A_NC_038235.1.gb"
-# snp_db <- dplyr::select(snp_db, SNP)
-# snps.to.amino(snp_db, ref_seq)
+# # snp_db <- fasta.to.snps(fasta="/scratch/tporter/RSV_20230402_SequencingMethodCompare_SQL/RSV_A_Fasta/TGen_Clinical_RSVA.fasta",
+# #                         ref_seq="/scratch/tporter/RSV_20230630_Phylogenetic_Analysis/",
+# #                         cores=parallelly::availableCores())
+# # snp_db <- read.csv("/scratch/tporter/RSV_20230908_HomoplasticSNPAnalysis/RSV_A_Homoplastic.csv")
+# # ref_seq="/scratch/tporter/RSV_20230825_ROI_Formatting/RSV_A_NC_038235.1.gb"
+# # snp_db <- dplyr::select(snp_db, SNP)
+# # snps.to.amino(snp_db, ref_seq)
+#
+# ref_seq <- "/scratch/mfolkerts/references/H37Rv_NC0009623.gb"
+# snp_db <- data_frame("SNP" = c("C160780G", "C160451A", "T491742C"))
+# cores = 3
+
